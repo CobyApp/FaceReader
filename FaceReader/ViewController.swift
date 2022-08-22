ef37d498a28e856370f6d6ad6cd66ca676d1e6bd
kimdoyoung <57849386+coby5502@users.noreply.github.com>
2022-08-22 16:31:27 +0900

    Initial Commit

---
+++ FaceReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceReader
+//
+//  Created by COBY_PRO on 2022/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
